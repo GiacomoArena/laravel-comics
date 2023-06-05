e40d1c47af667a49eea6c8fb801700a797770684
GiacomoArena <giacomoarena28@gmail.com>
2023-06-05 18:06:13 +0200

    make the comics menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,6 @@
+@php
+    $comics = config('comics')
+@endphp
 @extends('layout.main')
 
 @section('content')
@@ -9,7 +12,14 @@
             <h4>current series</h4>
         </span>
         <div class="container">
+        @foreach ($comics as $comic)
 
+            <div class="props-container">
+                <img src="{{$comic["thumb"]}}" alt="">
+                <h6 class="comics_font">{{$comic["series"]}}</h6>
+            </div>
+
+        @endforeach
         </div>
         <span>
             <h5>load more</h5>
