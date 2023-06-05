a233e60a9aa2f2d4b58f8ecb16bd3c7213adc4a0
GiacomoArena <giacomoarena28@gmail.com>
2023-06-05 16:34:53 +0200

    uppdate commit

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,20 @@
 <header>
-    header
+
+    <div class="container">
+        <section class="logo">
+            <img src="/img/dc-logo.png" alt="logo">
+        </section>
+
+        <section class="links">
+            <ul>
+                <li>
+                    <a>
+                        ciao
+                    </a>
+
+                </li>
+            </ul>
+        </section>
+
+    </div>
 </header>
